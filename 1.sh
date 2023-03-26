88675a6b9ede05802195e9dc093ff5583249e4af
lesixmail <37403363+lesixmail@users.noreply.github.com>
2023-03-26 16:28:17 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -4,9 +4,12 @@
 systemctl stop firewalld
 systemctl disable firewalld
 
-# 添加DNS 8.8.8.8
+# 添加DNS 8.8.8.8等
 echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf > /dev/null
 echo "nameserver 114.114.114.114" | sudo tee -a /etc/resolv.conf > /dev/null
 echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf > /dev/null
 echo "nameserver 223.5.5.5" | sudo tee -a /etc/resolv.conf > /dev/null
+
+# 更新各种包
+sudo yum update
 
