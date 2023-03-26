8257c385f2fa1d2c9979311e961a9fe60a124014
lesixmail <37403363+lesixmail@users.noreply.github.com>
2023-03-26 16:23:20 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -5,7 +5,8 @@ systemctl stop firewalld
 systemctl disable firewalld
 
 # 添加DNS 8.8.8.8
-echo "nameserver 1.1.1.1" >> /etc/resolv.conf
-echo "nameserver 114.114.114.114" >> /etc/resolv.conf
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-echo "nameserver 223.5.5.5" >> /etc/resolv.conf
+echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf > /dev/null
+echo "nameserver 114.114.114.114" | sudo tee -a /etc/resolv.conf > /dev/null
+echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf > /dev/null
+echo "nameserver 223.5.5.5" | sudo tee -a /etc/resolv.conf > /dev/null
+
