3b399e27d78376cd1386ab84cd5db36f6470c7ad
lesixmail <37403363+lesixmail@users.noreply.github.com>
2023-03-26 16:49:54 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -11,7 +11,7 @@ echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf > /dev/null
 echo "nameserver 223.5.5.5" | sudo tee -a /etc/resolv.conf > /dev/null
 
 # 更新各种包
-sudo yum update -y
+# sudo yum update -y
 
 # 安装后端
 wget -N https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh && bash install.sh
