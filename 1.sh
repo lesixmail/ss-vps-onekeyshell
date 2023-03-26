9a866d7ade2e7817fc91be5752afb660e66ad26d
lesixmail <37403363+lesixmail@users.noreply.github.com>
2023-03-26 16:41:13 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -13,6 +13,14 @@ echo "nameserver 223.5.5.5" | sudo tee -a /etc/resolv.conf > /dev/null
 # 更新各种包
 sudo yum update -y
 
+# 安装后端
+wget -N https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh && bash install.sh
+
+wget --no-check-certificate -O bbr.sh https://github.com/teddysun/across/raw/master/bbr.sh && \
+chmod +x bbr.sh && \
+echo 3 | sudo ./bbr.sh
+
+# bbrplus以及内核
 wget --no-check-certificate -O bbrplus_auto.sh https://github.com/cx9208/Linux-NetSpeed/raw/master/tcp.sh && chmod +x bbrplus_auto.sh && echo 1 | sudo ./bbrplus_auto.sh
 sudo reboot
 lsmod | grep bbr
