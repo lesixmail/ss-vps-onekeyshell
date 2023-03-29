691ba42690521e163438d595d007afa500751c0b
lesixmail <37403363+lesixmail@users.noreply.github.com>
2023-03-29 23:13:19 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -11,12 +11,16 @@ echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf > /dev/null
 echo "nameserver 223.5.5.5" | sudo tee -a /etc/resolv.conf > /dev/null
 
 # 更新各种包
-# sudo yum update -y
+sudo yum update -y
+
+# 安装screen
+yum install screen -y
 
 # 安装后端
 wget -N https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh && bash install.sh
 
-
 # 安装bbr
-bash <(curl -Lso- https://git.io/kernel.sh) 
+echo "1" | bash <(curl -Lso- https://git.io/kernel.sh)
+echo "61" | bash <(curl -Lso- https://git.io/kernel.sh)
+
 
